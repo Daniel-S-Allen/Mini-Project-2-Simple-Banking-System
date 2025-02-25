cfecd1006d39f37e0d1d98128dd67b564e59b12e
Johnathan Lavoie <johnathan.lavoie@ontariotechu.net>
2025-02-25 09:20:04 -0500

    Minor modifications to account and display function

--- Account.py
+++ Account.py
@@ -1,6 +1,6 @@
-class bank:
-    def __init__(self, account, balance):
-        self.account = account
+class Account:
+    def __init__(self, id, balance):
+        self.id = id
         self.list_of_transectios = {}
         self.balance = balance
     
@@ -9,32 +9,34 @@ class bank:
     
     def withdraw(self, with_am, desc):
         if with_am > self.balance:
-            print('insufferable balance!!')
+            raise ValueError("You dont have enough balance to withdraw that ammount")
         else:
             self.balance -= with_am
-            bank.update_transection(desc, with_am)
+            self.update_transection(desc, with_am)
 
     def deposit (self, dep_am, desc):
         if dep_am < 0:
-            print("you cant add negative ammount!!!")
+            raise ValueError("You dont have enough balance to withdraw that ammount")
         else:
             self.balance += dep_am
-            bank.update_transection(desc, dep_am)
+            self.update_transection(desc, dep_am)
     
     def display(self):
         # Get the maximum length of the strings to format the output
-        length = max(len(f"Account Balance: {self.balance}"), f"Id: {self.id}", "Transaction History", [f"{transaction['description']}: {transaction['amount']}" for transaction in self.transactions.items()])
-        
+        length = max(len(f"Account Balance: {self.balance}"), len(f"Id: {self.id}"), len("Transaction History"), *[len(f"{transaction[0]}: {transaction[1]}") for transaction in self.list_of_transectios.items()])
+    
         # Print the output with the correct formatting
         print("-" * length)
         print("Account Details")
         print("-" * length)
         print(f"Account Balance: {self.balance}")
         print(f"Id: {self.id}")
+        if len(self.list_of_transectios) == 0:
+            print("No transactions yet.")
         print()
         print("-" * length)
         print("Transaction History")
         print("-" * length)
-        for transaction in self.transactions:
-            print(f"{transaction['description']}: {transaction['amount']}")
+        for transaction in self.list_of_transectios.items():
+            print(f"{transaction[0]}: {transaction[1]}")
         print("-" * length)
