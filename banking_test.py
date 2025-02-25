0fdba3f033e8bc0913cb758a5d4dfbb9babd351a
Daniel Allen <danielsimeallen@gmail.com>
2025-02-25 09:32:38 -0500

    Added basic pytest

---
+++ banking_test.py
@@ -0,0 +1,41 @@
+from math import inf, pi
+import Account 
+import pytest
+def test_deposit():
+    account = Account.Account("Name1", 0)
+    # test for 'reasonable' input
+    account.deposit(10)
+    assert(account.balance() == 10), "Should be 10"
+    account.deposit(11.5)
+    assert(account.balance() == 21.5), "Should be 21.5"
+    account.deposit(pi)
+    assert(account.balance() == (21.5+pi)), "Should be 24.64159265"
+
+    # test for malicious input
+    with pytest.raises(Exception):
+        account.withdraw(-5)
+    with pytest.raises(Exception):
+        account.deposit(inf)
+    with pytest.raises(Exception):
+        account.deposit("test")
+
+def test_withdraw():
+    account = Account.Account
+    # test for 'reasonable' input
+    account.withdraw(10)
+    assert(account.balance() == 10)
+    account.withdraw(11.5)
+    assert(account.balance() == 21.5)
+    account.withdraw(pi)
+    assert(account.balance() == (21.5+pi))
+    
+    # test for malicious input
+    with pytest.raises(Exception):
+        account.withdraw(-5)
+    with pytest.raises(Exception):
+        account.withdraw(inf)
+    with pytest.raises(Exception):
+        account.withdraw("test")
+
+test_deposit()
+test_withdraw()
