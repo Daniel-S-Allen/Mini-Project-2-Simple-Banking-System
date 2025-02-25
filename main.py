dd0d1b573779af602a9fce9134b1b2a3ffde0c59
Johnathan Lavoie <johnathan.lavoie@ontariotechu.net>
2025-02-25 08:48:34 -0500

    Changed ID to be set instead of inputted repeatedly

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 from Account import Account 
 
 accounts = []
+current_account = None 
 
 while True:
-    action = input("What would you like to do? (create, withdraw, deposit, view, quit): ")
+    action = input("What would you like to do? (create, select, withdraw, deposit, view, quit): ")
 
     if action == "create":
-        id = input("Enter the ID: ")
-        balance = input("Enter the initial balance: ")
+        id = int(input("Enter the ID: "))
+        balance = int(input("Enter the initial balance: "))
         if id in [account.id for account in accounts]:
             print("Account with that ID already exists.")
             continue
-        accounts.append(Account(id=int(id), balance=int(balance)))
+        accounts.append(Account(id=id, balance=balance))
+        current_account = accounts[-1]
+    
+    elif action == "select":
+        id = int(input("Enter the ID: "))
+        for account in accounts:
+            if account.id == id:
+                current_account = account
+                break
+        else:
+            print("Error: Account with that ID does not exist.")
+
+    elif current_account is None:
+        print("No account selected. Please create or select an account first.")
     
     elif action == "withdraw":
-        id = input("Enter the ID: ")
         amount = input("Enter the amount to withdraw: ")
-        for account in accounts:
-            if account.id == int(id):
-                account.withdraw(int(amount))
-                continue
-        print("No account with that ID exists.")
+        try:
+            current_account.withdraw(int(amount))
+        except:
+            print("Error: Invalid withdraw amount.")
         
     
     elif action == "deposit":
-        id = input("Enter the ID: ")
         amount = input("Enter the amount to deposit: ")
-        for account in accounts:
-            if account.id == int(id):
-                account.deposit(int(amount))
-                continue
-        print("No account with that ID exists.")
+        current_account.deposit(int(amount))
     
     elif action == "view":
-        id = input("Enter the ID: ")
-        for account in accounts:
-            if account.id == int(id):
-                account.display()
-                continue
-        print("No account with that ID exists.")
+        amount = input("Enter the amount to deposit: ")
+        current_account.display(int(amount))
     
     elif action == "quit":
         for account in accounts:
